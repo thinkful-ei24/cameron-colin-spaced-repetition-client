678c0475c45b57fa348581dbe7445e51fa1888b3
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-28 13:14:41 -0700

    added styling to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -42,7 +42,7 @@ export class Dashboard extends React.Component {
     //   this.props.dispatch(incorrectGuess());
     // }
     this.props.dispatch(updateData());
-    this.setState({translation: '', submitted: true});
+    this.setState({ translation: '', submitted: true });
   }
 
   handleChange = (e) => {
@@ -60,7 +60,7 @@ export class Dashboard extends React.Component {
 
 
   render() {
-  console.log(this.props.protectedData)
+    console.log(this.props.protectedData)
     let nameDisplay;
     if (this.newLogIn) {
       nameDisplay = <div className="name"><p>Welcome {this.props.username}!</p></div>
@@ -70,46 +70,50 @@ export class Dashboard extends React.Component {
       cardContent = <p>
         {`${this.props.protectedData.question.toUpperCase()} / ${this.props.protectedData.answer.toUpperCase()}`}
       </p>
-      } else {
-        cardContent = <p>
-          {this.props.protectedData.question.toUpperCase()}
-        </p>
-      }
-      let percentageCorrect;
-      if(this.props.protectedData.guesses === 0){
-        percentageCorrect = 'No score yet for this question';
-      }else{
-        let percent = Math.round(this.props.protectedData.correct/this.props.protectedData.guesses*100);
-        percentageCorrect = `Percentage Correct for this question: ${percent}%`;
-      }
-      console.log(this.props.feedback);
+    } else {
+      cardContent = <p>
+        {this.props.protectedData.question.toUpperCase()}
+      </p>
+    }
+    let percentageCorrect;
+    if (this.props.protectedData.guesses === 0) {
+      percentageCorrect = 'No score yet for this question';
+    } else {
+      let percent = Math.round(this.props.protectedData.correct / this.props.protectedData.guesses * 100);
+      percentageCorrect = `Percentage Correct for this question: ${percent}%`;
+    }
+    console.log(this.props.feedback);
 
-      return (
-        <main role="main" className="dashboard row">
-          {nameDisplay}
-          <div className="card-container">
+    return (
+      <main role="main" className="dashboard row">
+        {nameDisplay}
+        <div className="card-container">
           <div className="percentage">{percentageCorrect}</div>
-            <div className={`flashcard ${this.props.feedback}`}>
-              {cardContent}
-            </div>
+          <div className={`flashcard ${this.props.feedback}`}>
+            {cardContent}
           </div>
-          <form className="answer-form" onSubmit={(e) => this.handleSubmit(e)}>
-            <label htmlFor="answer">
-              <input
-                type="text"
-                name="answer"
-                id="answer"
-                placeholder="english translation"
-                value={this.state.translation}
-                onChange={this.handleChange} />
-            </label>
+        </div>
+        <form className="answer-form" onSubmit={(e) => this.handleSubmit(e)}>
+          <div className="input-holder">
+          <label htmlFor="answer">
+            <input
+              type="text"
+              name="answer"
+              id="answer"
+              placeholder="english translation"
+              value={this.state.translation}
+              onChange={this.handleChange} />
+          </label>
+          </div>
+          <div className="button-holder">
             <button type="submit" disabled={this.state.submitted}>Submit</button>
             <button type="button" disabled={!this.state.submitted} onClick={() => this.skipButton()}>Next</button>
-          </form>
-        </main>
-      );
-    }
-    }
+          </div>
+        </form>
+      </main>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   //const { currentUser } = state.auth;
