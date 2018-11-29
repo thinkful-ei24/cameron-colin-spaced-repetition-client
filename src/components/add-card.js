9df6138826a11bc32e68752717e7a0e437c60286
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-29 13:00:37 -0700

    added styling and form to add card component

--- src/components/add-card.js
+++ src/components/add-card.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import requiresLogin from './requires-login';
+import './add-card.css';
 
-export default class AddCard extends React.Component{
+export class AddCard extends React.Component{
+
+  addCard(){
+    console.log('clicked');
+  }
   render(){
+    console.log('hello');
     return (
-      <form className="add-card-form">
+      <div className="add-card">
+        <h2>Add a new card to practice!</h2>
+      <form className="add-card-form" onSubmit={() => this.addCard()}>
         <label htmlFor="spanish">Spanish:</label>
+        <br/>
         <input type="text" id="spanish" name="spanish"></input>
+        <br/>
         <label htmlFor="english">English</label>
+        <br/>
         <input type="text" id="english" name="english"></input>
+        <br/>
+        <button type="submit" className="add-button">Add Card</button>
       </form>
-    )
+      </div>
+    );
   }
 }
+
+export default requiresLogin()(AddCard);
 
 
 
